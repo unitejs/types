b6758c620bcd1e81eb09242772084d6f291d2b58
Martyn Janes <martyn@obany.com>
2017-11-01 16:13:22 +0300

    Added pwa flag

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,16 @@
 interface Unite {
-    /** Configuration parameters specified in your UniteJS build configurations*/
+    /** Configuration parameters specified in your UniteJS build configurations */
     config: { [id: string]: any };
     /** The name of the configuration. */
     configName: string;
     /** The packageVersion of the app. */
     packageVersion: string;
     /** Is the build bundled. */
-    bundle: string;
+    bundle: boolean;
     /** Is the build minified. */
-    minify: string;
+    minify: boolean;
+    /** Is the build pwa. */
+    pwa: boolean;
     /** UniteJS framework version */
     uniteVersion: string;
 }
